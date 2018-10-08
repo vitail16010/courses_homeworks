0565bd2838f5145830ae1899b902a7b334ef136a
Realmeteor <43382288+Realmeteor@users.noreply.github.com>
2018-10-08 11:37:54 +0300

    Bolbas (#14)
    home work 1.1 1.2

---
+++ bolbas_hw/hw1_1.py
@@ -0,0 +1,3 @@
+import random
+a = (random.randint(0, (int(input())))),random.randint(0,(int(input())))
+print (a)
---
+++ bolbas_hw/hw1_2.py
@@ -0,0 +1,5 @@
+import random
+a = (random.randint(1000, 100000))
+b = 1000
+c =10
+print ((a), a // b % c)
